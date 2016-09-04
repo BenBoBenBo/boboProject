abc5acb11712a4d9b7743ea6b109f5b8ffb8b652
sethbowers <seth.m.bowers@outlook.com>
2016-09-04 15:55:53 -0600

    added project hw to repo

---
+++ helloworlds.cpp
@@ -0,0 +1,14 @@
+// helloworlds.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+
+int main()
+{
+	printf("Hello worlds!\n");
+    return 0;
+}
+
